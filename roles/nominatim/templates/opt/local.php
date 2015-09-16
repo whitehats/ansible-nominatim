137f0f43a822f01dbec2a0b098d3a74abbe245fd
Marek Hobler <marek@whitehats.pl>
2015-09-16 16:58:43 +0200

    Initial draft for debian stable

---
+++ roles/nominatim/templates/opt/local.php
@@ -0,0 +1,8 @@
+<?php
+  // Paths
+   @define('CONST_Postgresql_Version', '9.4');
+   @define('CONST_Postgis_Version', '2.1');
+   
+   // Website settings
+   @define('CONST_Website_BaseURL', 'http://change_me/');
+   
